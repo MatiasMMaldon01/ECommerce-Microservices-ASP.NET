a8d6b32a29a0aea71e42f58cb02a8bfdbe7fa46f
MatiasMMaldon01 <matiasm843@gmail.com>
2023-06-21 16:27:55 -0300

    Basket API

---
+++ src/Services/Basket/Api.Basket/Controllers/BasketController.cs
@@ -0,0 +1,50 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Api.Basket.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+            if (basket == null)
+            {
+                return NotFound("Opss...There are no items here...");
+            }
+
+            return Ok(basket);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            await _repository.UpdateBasket(basket);
+            return Ok("Items from Basket deleted succesfully");
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await _repository.DeleteBasket(userName);
+            return Ok("Items from Basket created succesfully");
+        }
+
+    }
+}
